e38585daad8d2c686235c8f97da164d8d6971be8
Andreas Guther <andreas@guther.family>
2020-12-15 10:08:25 +0100

    [FG] Add cross track error and track angle error SimVars

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -2941,6 +2941,7 @@ class FMCMainDisplay extends BaseAirliners {
     Init() {
         super.Init();
         this.dataManager = new FMCDataManager(this);
+        this._flightGuidance = new NXFlightGuidance(this);
         this.tempCurve = new Avionics.Curve();
         this.tempCurve.interpolationFunction = Avionics.CurveTool.NumberInterpolation;
         this.tempCurve.add(-10 * 3.28084, 21.50);
@@ -3255,6 +3256,9 @@ class FMCMainDisplay extends BaseAirliners {
             SimVar.SetSimVarValue("L:AIRLINER_MANAGED_APPROACH_SPEED", "number", this.getManagedApproachSpeed());
         }
         this.updateRadioNavState();
+        if (this._flightGuidance) {
+            this._flightGuidance.update(_deltaTime);
+        }
     }
 
     onEvent(_event) {
